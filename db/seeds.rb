c8401f1d10a3537e4981a313dcfdce0241dff7aa
hk-cogoport <kumarharshit8090@gmail.com>
2023-11-29 19:04:07 +0530

    seeded_data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Seed data for colleges
+college_names = ['Stanford University', 'University of Tokyo', 'Oxford University', 'MIT', 'Harvard University']
+colleges = college_names.map { |name| College.create(name: name) }
+
+# Seed data for exams
+exam_data = [
+  { exam_name: 'Mathematics Test', start_time_offset: -2.days, end_time_offset: 2.days },
+  { exam_name: 'Physics Exam', start_time_offset: -1.day, end_time_offset: 3.days },
+  { exam_name: 'Literature Exam', start_time_offset: -3.hours, end_time_offset: 2.days },
+  { exam_name: 'Biology Test', start_time_offset: -1.hour, end_time_offset: 1.day },
+  { exam_name: 'Computer Science Quiz', start_time_offset: -4.hours, end_time_offset: 1.day },
+  { exam_name: 'Chemistry Exam', start_time_offset: -2.days, end_time_offset: 1.day }
+]
+
+colleges.each do |college|
+  exam_data.each do |exam_info|
+    exam = Exam.create(college: college, exam_name: exam_info[:exam_name])
+    start_time = Time.now + exam_info[:start_time_offset]
+    end_time = Time.now + exam_info[:end_time_offset]
+    ExamWindow.create(exam: exam, start_time: start_time, end_time: end_time)
+  end
+end
+
